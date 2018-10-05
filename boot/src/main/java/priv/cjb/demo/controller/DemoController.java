7f4f381dfa6a691401276d09473e78e2814ccc23
Your Name <you@example.com>
2018-10-05 18:00:52 +0800

    配置服务调试

--- boot/src/main/java/priv/cjb/demo/controller/DemoController.java
+++ boot/src/main/java/priv/cjb/demo/controller/DemoController.java
@@ -3,6 +3,7 @@ package priv.cjb.demo.controller;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,11 +11,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping
 public class DemoController {
 
+	@Value("${name}")
+	public String port;
+
 	@RequestMapping("demo")
 	public List<String> demo() {
 		List<String> list = new ArrayList<>();
 		list.add("hello");
 		list.add("boy");
+		list.add(port);
 		return list;
 	}
 }
